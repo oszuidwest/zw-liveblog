ec30a630a2e87066099a3805592fc47dffced0f2
rmens <r.mens@me.com>
2026-05-10 23:14:05 +0200

    chore: align phpcs baseline

--- uninstall.php
+++ uninstall.php
@@ -3,17 +3,19 @@
  * Uninstall handler for ZuidWest Liveblog plugin.
  *
  * Removes all plugin transients from the database when the plugin is deleted.
+ *
+ * @package ZuidWestLiveblog
  */
 
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    exit;
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
 }
 
 global $wpdb;
 $wpdb->query(
-    $wpdb->prepare(
-        'DELETE FROM ' . $wpdb->options . ' WHERE option_name LIKE %s OR option_name LIKE %s',
-        '_transient_zw_liveblog_%',
-        '_transient_timeout_zw_liveblog_%'
-    )
+	$wpdb->prepare(
+		'DELETE FROM ' . $wpdb->options . ' WHERE option_name LIKE %s OR option_name LIKE %s',
+		'_transient_zw_liveblog_%',
+		'_transient_timeout_zw_liveblog_%'
+	)
 );
